33f6374922677536396d159ad7f2702ba4f017aa
avanti <avanti.d@amazatic.com>
2018-12-28 12:15:20 +0530

    added new functions in base page

---
+++ pages/basepage.py
@@ -0,0 +1,56 @@
+# import sys
+# sys.path.append('../locators')
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from faker import Factory
+from locators.sign_in_page_locators.signin_buyer_supplier_locator import SignInLocators
+import time
+
+fake = Factory.create()
+
+
+class BasePage(object):
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def wait_for_element(self, *locator):
+        " Performs wait for time provided"
+        WebDriverWait(self.driver, 15).until(
+            EC.visibility_of_element_located(*locator))
+
+    def wait_for_element_clickable(self, *locator):
+        " wait for element to be clickable "
+        WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable(*locator))
+
+    def wait_for_element_presence(self, *locator):
+        WebDriverWait(self.driver, 15).until(
+            EC.presence_of_element_located(*locator))
+
+    def wait_for_text_in_element(self, *locator, actual_text):
+        WebDriverWait(self.driver,15).until(EC.text_to_be_present_in_element(*locator), actual_text)
+
+    def get_chat(self):
+        return self.driver.find_element(*SignInLocators.chat)
+
+    def close_chat_popup(self):
+        self.driver.switch_to.frame(self.get_chat())
+        self.driver.find_element(*SignInLocators.close_chat).click()
+        self.driver.switch_to.default_content()
+        time.sleep(2)
+
+    def close_chat_popup_while_button_click(self, button):
+        try:
+            button.click()
+        except:
+            self.close_chat_popup()
+            button.click()
+            '''
+            try:
+                button.click()
+            except:
+                self.close_chat_popup()
+                button.click()
+            '''
