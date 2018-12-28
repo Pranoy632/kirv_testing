b7257be0140cf57dca002dde65cdf39bbb8ef6c2
avanti <avanti.d@amazatic.com>
2018-12-28 11:46:01 +0530

    separated locators according to their respective pages.

---
+++ locators/buyer_locators/homepage_locators.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+
+
+class HomePageLocators(object):
+
+    search_bar = (By.CSS_SELECTOR, 'body > app-root > product > header > header-options >'
+                                   ' div > div > div.col-xl-4.col-lg-5.col-md-5.col-12.search-box-m > form > input')
+    categories_link = (By.ID, 'navDropDown-1')
+    category_dropdpwn = (By.CSS_SELECTOR, 'div.show > div:nth-child(1) > div:nth-child(1) > div.row.product-list')
+    total_categories = (By.CSS_SELECTOR, 'div.show > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)')
+    single_category = (By.CSS_SELECTOR, 'div.col-sm-4')
+    category_name = (By.CSS_SELECTOR, 'h3:nth-child(2)')
+    no_of_products_categories_list = (By.CSS_SELECTOR, 'div:nth-child(3)')
+    cart = (By.CLASS_NAME, 'cart-outer')
+    loader = (By.CLASS_NAME, 'centered')
+
+
+class AllProductsLocators(object):
+    panel_title = (By.CLASS_NAME, 'panel-title')
+    panel_no_of_products = (By.CLASS_NAME, 'm-0')
+    breadcrumb = (By.CLASS_NAME, 'breadcrumb-page')
+    breadcrumb_links_total = (By.TAG_NAME, 'a')
+    page_menu = (By.CLASS_NAME, 'page-title-menu')
+    all_menus = (By.TAG_NAME, 'li')
+    sorting_by_price = (By.CSS_SELECTOR, 'div.dropdown:nth-child(2)')
+    sorting_by_warehouse = (By.CSS_SELECTOR, 'div.dropdown:nth-child(2) > a:nth-child(1)')
+    low_to_high = (By.ID, 'customCheck21')
+    high_to_low = (By.ID, 'customCheck22')
+    product_list = (By.CSS_SELECTOR, 'div.product-list:nth-child(4)')
+    single_product = (By.CSS_SELECTOR, 'div.col-lg-4')
+    cost = (By.CSS_SELECTOR, 'div:nth-child(3) > span:nth-child(1)')
+    product_column = (By.CSS_SELECTOR, 'div.col-lg-4')
+    product_cell_price = (By.CSS_SELECTOR, 'div:nth-child(3) > span:nth-child(1)')
+    sort_lower = (By.ID, 'customCheck21')
+    sort_higher = (By.ID, 'customCheck22')
+    sort_apply_button = (By.CSS_SELECTOR, 'button.btn:nth-child(3)')
+    loader = (By.CLASS_NAME, 'centered')
+
---
+++ locators/buyer_locators/landing_page_locators.py
@@ -0,0 +1,32 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginLocators:
+    email = (By.NAME, 'username')
+    password = (By.NAME, 'password')
+    sign_in = (By.CSS_SELECTOR, 'body > div > form > button')
+    signin_kirv_image = (
+        By.CSS_SELECTOR, 'ng-component > div.container-fluid > div > div:nth-child(1) > div > div.logo > img')
+
+    signin_title = (
+        By.CSS_SELECTOR, 'ng-component > div.container-fluid > div > div:nth-child(1) > div > h1')
+
+    email_login = (
+        By.CSS_SELECTOR,
+        'ng-component > div.container-fluid > div > div:nth-child(1) > div > form > fieldset > field:nth-child(1) > div > label > input')
+
+    pwd_login = (By.CSS_SELECTOR,
+                 'ng-component > div.container-fluid > div > div:nth-child(1) > div > form > fieldset > field:nth-child(2) > div > label > input')
+
+    email_login_error = (
+        By.CSS_SELECTOR,
+        'ng-component > div.container-fluid > div > div:nth-child(1) > div > form > fieldset > field:nth-child(1) > div > span')
+
+    pwd_login_error = (
+        By.CSS_SELECTOR,
+        'ng-component > div.container-fluid > div > div:nth-child(1) > div > form > fieldset > field:nth-child(2) > div > span')
+
+    signin_login_btn = (
+        By.CSS_SELECTOR, 'ng-component > div.container-fluid > div > div:nth-child(1) > div > form > fieldset > button')
+
+
